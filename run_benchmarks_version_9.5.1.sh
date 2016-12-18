2f3071fc7e5bb18f5eeeb22d9ff1b2a9c40a1573
zecarlos94 <zecarlos@MBP-de-Jose-8.lan>
2016-12-18 11:40:14 +0000

    .sh done and working fully

--- run_benchmarks_version_9.5.1.sh
+++ run_benchmarks_version_9.5.1.sh
@@ -24,16 +24,17 @@ do
   pg_ctl -D Databases/abd reload
   wait $!
 
-  # Run benchmark
+  # Create folder for current iteration
   mkdir $file
+
+  # Run benchmark
   cd Benchmark
-  ./oltpbenchmark -b epinions -c config/abd.xml --create=true --load=true --execute=true -s 5 -o $file/$file
+  ./oltpbenchmark -b epinions -c config/abd.xml --create=true --load=true --execute=true -s 5 -o ../$file/$file
   cd ..
-  sleep 5
+  sleep 10
 
   # Run pgbadger
-  pgbadger Databases/abd/pg_log/postgresql.log
-  cp Databases/abd/pg_log/out.html $file/$file.html
+  pgbadger Databases/abd/pg_log/postgresql.log -o $file.html -O $file/
   rm Databases/abd/pg_log/*.log
 	
 done
